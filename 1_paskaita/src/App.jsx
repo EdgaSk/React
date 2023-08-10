94370d6d00bcbcd88d1e628a5d36ecbb45b58446
edgaras.skucas@gmail.com <132943373+EdgaSk@users.noreply.github.com>
2023-08-10 20:59:29 +0300

    new files

---
+++ 1_paskaita/src/App.jsx
@@ -0,0 +1,54 @@
+const App = () => {
+
+  return(
+    <div className="mainBlock">
+       <nav className="navCointainer">
+          <img src="src/assets/react.svg" alt="" />
+          <div>
+            <li>Home</li>
+            <li>Product</li>
+            <li>Company</li>
+            <li>Contact</li>
+          </div>
+       </nav>
+       <main>
+          <div className="headerImg">
+              <h1>Header Image</h1>
+          </div>
+          <div className="aboutBlock">
+              <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora, consequatur totam adipisci error cupiditate! Similique, eligendi, officiis est architecto odio commodi molestiae ut sit voluptatibus repellendus earum saepe debitis!</p>
+              </div>
+              <div>
+                <h1>Company</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora, consequatur totam adipisci error cupiditate! Similique, eligendi, officiis est architecto odio commodi molestiae ut sit voluptatibus repellendus earum saepe debitis!</p>
+              </div>
+              <div>
+                <h1>Services</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora, consequatur totam adipisci error cupiditate! Similique, eligendi, officiis est architecto odio commodi molestiae ut sit voluptatibus repellendus earum saepe debitis!</p>
+              </div>
+          </div>
+          <div className="contentBlock">
+            <div className="mainContent">
+                <h1>Content</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit laborum voluptatem doloribus aliquam adipisci dolores architecto molestias sequi? Voluptatem omnis iure tempora mollitia quidem repudiandae magni iusto veniam nisi natus?</p>
+                <h2>Sub Header</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit laborum voluptatem doloribus aliquam adipisci dolores architecto molestias sequi? Voluptatem omnis iure tempora mollitia quidem repudiandae magni iusto veniam nisi natus?</p>
+            </div>
+            <div className="sideNav">
+              <h2>Navigation</h2>
+              <div className="menuLinks">
+                <li>Home</li>
+                <li>Product</li>
+                <li>Company</li>
+                <li>Contact</li>
+              </div>
+            </div>
+          </div>
+       </main>
+    </div>
+  );
+}
+
+export default App;
